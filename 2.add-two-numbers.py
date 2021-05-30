bdc162b541845cea4b439836d6dc6bfe5ed300e0
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-05-30 09:11:51 +0900

    写経

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -10,15 +10,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from typing import List
+
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
 
-        left = l1
-        right = l2
-        while left != None and right != None:
-            print(left.val + right.val)
-            left = left.next
-            right = right.next
+        result = tmp = ListNode(0)
+        carry = 0
+
+        while l1 or l2:
+            sum_value = carry
+            if l1 != None:
+                sum_value += l1.val
+                l1 = l1.next
+            if l2 != None:
+                sum_value += l2.val
+                l2 = l2.next
+            carry = sum_value // 10
+            tmp.next = ListNode(sum_value % 10)
+            tmp = tmp.next
+
+            if carry == 1:
+                tmp.next = ListNode(carry)
+        return result.next
 
 
 # @lc code=end
